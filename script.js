c0d2e88d80e7ff97fa63ead67c6d504e74691a5f
Rafael Modesto <rafaelmodestodafonseca@gmail.com>
2025-07-26 11:24:08 -0300

    Algumas implementações de calculo no javascript

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 /*
 TESTE! continuar amanhã!
 */
-function clicar() {
-    let resultado = window.document.getElementById('resultado');
-    resultado.value = "CLICOU!"
+/*
+function clicar(botao){
+    let resultadotela = document.getElementById('resultado');
+    resultadotela.value += botao.value;
 }
+*/
+    let resultadotela = document.getElementById('resultado');
+    let memoria = document.getElementById('memoria');
+    let botao = document.querySelectorAll('.botão');
+
+
+botao.forEach(function (botao){
+    botao.addEventListener('click', function(){
+        if(botao && botao.classList.contains('number')){
+        resultadotela.value += Number(botao.value);
+        memoria.value += Number(botao.value);
+    }   else if(botao.name == "apagar"){
+        resultadotela.value  = resultadotela.value.slice(0, -1);
+        memoria.value  = memoria.value.slice(0, -1);
+    } else if(botao.name == "apagartudo"){
+        resultadotela.value = null
+        memoria.value = null
+    }  else if(botao.name == "="){
+        resultadotela.value = eval(memoria.value);
+    } else { 
+        const valor = botao.dataset.real || botao.value
+        resultadotela.value += botao.value;
+        memoria.value += valor;
+    }
+    })})
