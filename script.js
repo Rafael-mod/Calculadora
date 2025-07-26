795c3026365f888c7b98bc73844e8278754e78eb
Rafael Modesto <rafaelmodestodafonseca@gmail.com>
2025-07-26 16:15:17 -0300

    Adicionais

--- script.js
+++ script.js
@@ -20,11 +20,26 @@ botao.forEach(function (botao){
     }   else if(botao.name == "apagar"){
         resultadotela.value  = resultadotela.value.slice(0, -1);
         memoria.value  = memoria.value.slice(0, -1);
-    } else if(botao.name == "apagartudo"){
+    }   else if(botao.name == "apagartudo"){
         resultadotela.value = null
         memoria.value = null
     }  else if(botao.name == "="){
-        resultadotela.value = eval(memoria.value);
+        resultadotela.value = eval(memoria.value) + "\n";
+        memoria.value = null
+    }   else if(botao.name == 'reciproco'){
+        resultadotela.value = null
+        resultadotela.value = eval(1 / memoria.value) + "\n"
+        memoria.value = null
+    }   else if(botao.name == 'elevado'){
+        resultadotela.value = null
+        resultadotela.value = eval(Math.pow(memoria.value, 2)) + "\n";
+        memoria.value = null
+    }   else if(botao.name == 'raiz'){
+        resultadotela.value == null
+        resultadotela.value= eval(Math.sqrt(memoria.value)) + "\n"; 
+        memoria.value = null
+    }   else if (botao.name == 'porcentagem'){
+         /* depois continuo */
     } else { 
         const valor = botao.dataset.real || botao.value
         resultadotela.value += botao.value;
